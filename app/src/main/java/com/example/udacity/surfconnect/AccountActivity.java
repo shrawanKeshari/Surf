f1b7c6bda8ed38f7194fd6508c856e0ebab81881
sonu <sonu190893@gmail.com>
2017-06-09 20:51:21 +0530

    successfully implemented passwordless login

--- app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
+++ app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.PhoneNumber;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
@@ -25,6 +32,41 @@ public class AccountActivity extends AppCompatActivity {
         id = (TextView) findViewById(R.id.id);
         infoLabel = (TextView) findViewById(R.id.info_label);
         info = (TextView) findViewById(R.id.info);
+
+        //once we are logged in via an account kit token, we can grab account information that we
+        // might be interested in such as account id, phone number or email. We can do this with
+        // call to getCurrentAccount(), in this we pass in anonymous inner class AccountKitCallback
+        // and make sure return type is account.
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+                //pass in account to grab all the information.
+                //Get Account Kit ID
+                String accountKitId = account.getId();
+                id.setText(accountKitId);
+
+                //Get Phone number and the check is the phone number is null.
+                PhoneNumber phonenumber = account.getPhoneNumber();
+                if(account.getPhoneNumber() != null){
+                    //if the phone number is available, display it.
+                    String formattedPhoneNumber = formatPhoneNumber(phonenumber.toString());
+                    info.setText(formattedPhoneNumber);
+                    infoLabel.setText(R.string.phone_label);
+                }else{
+                    //if the email address is available, display it.
+                    String emailString = account.getEmail();
+                    info.setText(emailString);
+                    infoLabel.setText(R.string.email_label);
+                }
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+                //display error
+                String toastMessage = accountKitError.getErrorType().getMessage();
+                Toast.makeText(AccountActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
