d3bf5b6cfaa1bf796ee2f29671881928c5459f4f
sonu <sonu190893@gmail.com>
2017-07-06 23:18:26 +0530

    launcher set and image display on loginactivity

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -639,11 +639,11 @@ public final class R {
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020050;
 		public static final int abc_textfield_search_material = 0x7f020051;
 		public static final int abc_vector_test = 0x7f020052;
-		public static final int design_fab_background = 0x7f02007c;
-		public static final int design_ic_visibility = 0x7f02007d;
-		public static final int design_snackbar_background = 0x7f02007e;
-		public static final int navigation_empty_icon = 0x7f02008c;
-		public static final int notification_template_icon_bg = 0x7f020097;
+		public static final int design_fab_background = 0x7f02007b;
+		public static final int design_ic_visibility = 0x7f02007c;
+		public static final int design_snackbar_background = 0x7f02007d;
+		public static final int navigation_empty_icon = 0x7f02008a;
+		public static final int notification_template_icon_bg = 0x7f020096;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0d00d8;
